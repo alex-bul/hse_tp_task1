6fcd0cfba35da3c833206076fc30e810c4b69234
Alexander Bulbenkov <bualal2005@gmail.com>
2024-04-21 23:58:25 +0300

    change file

--- copy.sh
+++ copy.sh
@@ -9,6 +9,8 @@ fi
 input_dir="$1"
 output_dir="$2"
 
+directories=$(find "$input_dir" -mindepth 1 -type d)
+
 # Проверяем, существует ли входная аудитория
 if [ ! -d "$input_dir" ]; then
     echo "Директория '$input_dir' не существует"
@@ -18,7 +20,29 @@ fi
 # Создание выходной директории, если она не существует
 mkdir -p "$output_dir"
 
-# Получение списка всех файлов
+# Получение списка файлов, находящихся непосредственно во входной директории
+files=$(find "$input_dir" -maxdepth 1 -type f)
+
+# Копирование файлов в выходную директорию
+for file in $files; do
+    filename=$(basename "$file")
+    if [ -e "$output_dir/$filename" ]; then
+        # Если файл с таким именем уже существует, добавляем к имени файла уникальный суффикс
+        # Пример: если в обеих аудиториях существуем main.py, то в выходную скопируется main-1.py.
+        # Если повторить действия с папками, то в выходной появится main-2.py и т.д.
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        counter=1
+        while [ -e "$output_dir/$filename-$counter.$extension" ]; do
+            counter=$((counter + 1))
+        done
+        cp "$file" "$output_dir/$filename-$counter.$extension"
+    else
+        cp "$file" "$output_dir/$filename"
+    fi
+done
+
+# Получение списка всех файлов, вложенных во входную директорию, и копирование их в выходную директорию
 nested_files=$(find "$input_dir" -type f)
 
 # Идем по списку файлов и каждый из них копируем
