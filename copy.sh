752975ea73e3f248b48fd4c5903406d2f3e21656
Alexander Bulbenkov <bualal2005@gmail.com>
2024-04-21 23:06:12 +0300

    Initial commit

---
+++ copy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Проверяем число аргументов. В случае ошибки - сообщаем пользователю и даём краткую инструкцию по работе со скриптом
+if [ "$#" -ne 2 ]; then
+    echo "Ошибка: неверное количество аргументов. Пример использования: $0 <входная_директория> <выходная_директория>"
+    exit 1
+fi
+
+input_dir="$1"
+output_dir="$2"
+
+# Проверяем, существует ли входная аудитория
+if [ ! -d "$input_dir" ]; then
+    echo "Директория '$input_dir' не существует"
+    exit 1
+fi
+
+# Создание выходной директории, если она не существует
+mkdir -p "$output_dir"
+
+# Получение списка всех файлов
+nested_files=$(find "$input_dir" -type f)
+
+# Идем по списку файлов и каждый из них копируем
+for file in $nested_files; do
+    filename=$(basename "$file")
+
+    # Проверка наличия файла с таким же именем в выходной директории
+    if [ -e "$output_dir/$filename" ]; then
+        # Если файл с таким именем уже существует, добавляем к имени файла уникальный суффикс
+        # Пример: если в обеих аудиториях существуем main.py, то в выходную скопируется main-1.py.
+        # Если повторить действия с папками, то в выходной появится main-2.py и т.д.
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        counter=1
+        while [ -e "$output_dir/$filename-$counter.$extension" ]; do
+            counter=$((counter + 1))
+        done
+        cp "$file" "$output_dir/$filename-$counter.$extension"
+    else
+        cp "$file" "$output_dir/$filename"
+    fi
+done
+
+echo "Копирование завершено успешно!"
